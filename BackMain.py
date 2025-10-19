2753c5cf1145fd8d74e0f48e8823a45ef11f0134
Divyansh Kachangal <dkachangal@gmail.com>
2025-10-19 13:24:59 +0530

    new temp

---
+++ BackMain.py
@@ -0,0 +1,13 @@
+def sum(a, b):
+    return a+b
+# a = input("Enter a ")
+# b = input("Enter b ")
+x = "HELLO"
+
+def strRev(x):
+    x = x[::-1]
+    return x
+
+print(strRev(x))
+
+# print(sum(a,b))
