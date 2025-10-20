45e187e8befbe7d8afa17ef2a5b2695a71f2d5ea
Divyansh Kachangal <dkachangal@gmail.com>
2025-10-21 00:41:15 +0530

    1 function done

--- backendMain.py
+++ backendMain.py
@@ -10,22 +10,31 @@ import datetime
 
 JpgimgPath = Path('characters')
 chars = {}
+
 # FUNCTION 2 Defines image in python + convert each image into image1.png + inserts it in dictionary chars
 def jpg2png():
-    str = "abcdefghijklmnopqrstuvexyz"
+
+    str = "abcdefghijklmnopqrstuvwxyz"
+
     for i in str:
         new = JpgimgPath/f'{i}.jpg'
-        img = Image.open(f"{i}.jpg").convert("L")
+        img = Image.open(new).convert("L")
+
         gray = np.array(img)
         thresh = threshold_otsu(gray)
         mask = gray < thresh
+
         rgba = np.zeros((gray.shape[0], gray.shape[1], 4), dtype=np.uint8)
         rgba[..., 0] = gray        # Red
         rgba[..., 1] = gray        # Green 
         rgba[..., 2] = gray        # Blue 
         rgba[..., 3] = mask * 255  #transparent
-         # save image:
+
+        # save image:
         # TO DO: 
         # update the code so as it creates a new folder and saves the image there rather than this location
-        Image.fromarray(rgba).save(f"{i}1.png")
+        Image.fromarray(rgba).save(f"PNG_{i}.png")
         chars[f"{i}"] = img
+
+# FUNCTION 3 to iterate the user defined string and paste each element on the page
+
